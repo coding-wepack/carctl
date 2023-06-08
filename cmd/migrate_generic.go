ac253aa1d5e6a57719d24310358bc19447d3d6c3
linyus <linyusheng@coding.net>
2023-06-08 15:45:13 +0800

    feat: if exists does not push

--- cmd/migrate_generic.go
+++ cmd/migrate_generic.go
@@ -54,7 +54,7 @@ func newMigrateGenericCmd(cfg *action.Configuration, out io.Writer) *cobra.Comma
 	cmd.Flags().IntVarP(&settings.Concurrency, "concurrency", "c", 1, "e.g., -c=2. Concurrency controls for how many artifacts can be pushed concurrently")
 	cmd.Flags().BoolVar(&settings.FailFast, "failFast", false, "exit directly if there was an error found during migration")
 	cmd.Flags().IntVar(&settings.MaxFiles, "max-files", -1, "Maximum number of files to be pushed. Negative number means unlimited.")
-	cmd.Flags().BoolVarP(&settings.Force, "force", "f", false, "whether push is forced. if exists does no push..")
+	cmd.Flags().BoolVarP(&settings.Force, "force", "f", false, "whether push is forced. if exists does no push.")
 
 	// TODO: --max-arts
 	// TODO: --generate-sha1
